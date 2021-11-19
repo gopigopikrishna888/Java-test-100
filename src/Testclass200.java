e31c7ebc69e3256f7fa067a321bb5c857a23abbb
gopigopikrishna888 <94610237+gopigopikrishna888@users.noreply.github.com>
2021-11-19 09:23:16 +0000

    Second commit...

---
+++ src/Testclass200.java
@@ -0,0 +1,13 @@
+
+public class Testclass200 {
+	public static void main(String[] args) {
+		char[] anArr;
+		anArr = new char[10];
+		anArr[0] = 'a';
+		anArr[1] = 'b';
+		System.out.println("Element at index 0 : " + anArr[0]);
+		System.out.println("Element at index 1 : " + anArr[1]);
+	}
+
+
+}
